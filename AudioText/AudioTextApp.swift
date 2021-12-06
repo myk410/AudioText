1a49a27e71ce17f177c8da8d30e2124f0ae489c1
myk410 <myk410@gmail.com>
2021-12-06 15:49:27 -0500

    Initial Commit

---
+++ AudioText/AudioTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTextApp.swift
+//  AudioText
+//
+//  Created by G. Michael Fortin Jr on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
